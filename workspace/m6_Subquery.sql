6edbe9105128d4754e4809a1d5bfe620121a6178
peien-tsai <59564956+peien-tsai@users.noreply.github.com>
2020-07-28 16:37:20 +0800

    加註解

--- workspace/m6_Subquery.sql
+++ workspace/m6_Subquery.sql
@@ -11,27 +11,28 @@ FROM employee
 WHERE title=(SELECT title
              FROM employee
              WHERE empno = 1002)
-		AND (SELECT salary
-			 FROM employee
+AND salary >(SELECT salary
+             FROM employee
              WHERE empno = 1005);
 
 -- P122 找出所有最低薪資比部門代號200最低薪資最高的部門alter
-SELECT deptno,Min(salary) 'Minimue Salary'
+SELECT deptno,Min(salary) 'Minimum Salary'
 FROM employee
 GROUP BY deptno
 HAVING MIN(salary)>(SELECT MIN(salary)
                    FROM employee
                    WHERE deptno = 200);
 select * from employee;
--- P123
+
+-- P123 【SELECT】找出部門代號為100的每位員工，其薪資戰部門代號為100所有員工薪資的比例
 SELECT ename,title,salary,
-		ROUND((salary*100/(SELECT SUM(salary)
-         FROM employee
-		 WHERE deptno =100 )),1) 'Percentage'
+	ROUND((salary*100/(SELECT SUM(salary)
+                           FROM employee
+		           WHERE deptno =100 )),1) 'Percentage'
 FROM employee
 WHERE deptno = 100 ;
 
--- P124
+-- P124 【FROM】找出部門代號為100的每位員工，其薪資戰部門代號號為100所有員工薪資的比例
 SELECT ename,title,salary,
 	   ROUND((salary*100/t.total),1)
 FROM employee,(SELECT SUM(salary) 'total'
@@ -39,7 +40,7 @@ FROM employee,(SELECT SUM(salary) 'total'
 		       WHERE deptno =100 )  t
 WHERE deptno = 100 ;
 
--- 
+-- 四捨五入
 SELECT round(123456.789) num;
 SELECT round(123456.789,1) num;
 SELECT round(123456.789,-1) num;
@@ -52,7 +53,7 @@ WHERE salary =(SELECT MIN(salary)
                FROM employee
                GROUP BY deptno) ;
 
--- P128
+-- P128  找出職稱不是senior engineer 且薪資比任何其中一個senior engineer 薪資來得低的員工
 SELECT ename,title,salary
 FROM employee
 WHERE salary < ANY
@@ -92,25 +93,25 @@ WHERE salary > ALL
 AND title<>'senior engineer';
 
 
--- 找出是主管的員工
+-- 找出是主管的員工 
 SELECT ename
 FROM emp
 WHERE empno IN (SELECT DISTINCT mgrno
 				FROM emp);
--- 找出所有不是主管的員工             
+-- 找出所有不是主管的員工           
 SELECT ename
 FROM emp
 WHERE empno NOT IN (SELECT DISTINCT mgrno
-				    FROM emp
-                    WHERE mgrno IS NOT NULL); 
+		    FROM emp
+                    WHERE mgrno IS NOT NULL);  -- (若有NULL值，Distinct 會包含NULL值)  
                     
--- Correlated Subquery
+-- Correlated Subquery(無法用在from子句上) 找出部門代號為100的每位員工，其薪資占部門代號為100所有員工薪資的百分比
 SELECT e.ename,e.title,e.salary,
          ROUND((salary*100/(SELECT SUM(salary)
-         FROM employee
-		 WHERE deptno =e.deptno )),1) 'Percentage'
+                            FROM employee
+		            WHERE deptno =e.deptno )),1) 'Percentage'
 FROM employee e
-WHERE deptno = 100 ;
+WHERE e.deptno = 100 ;
 
 -- P133 找出每個部門薪資最低的員工
 SELECT e.ename,e.salary,e.deptno
@@ -132,7 +133,7 @@ WHERE salary =(SELECT MIN(salary)
                FROM employee
                WHERE deptno=e.deptno) ; -- P134
                
--- P136
+-- P136 SELECT敘述中傳回的資料，直接新增到新表格中
 CREATE TABLE emp100
 SELECT empno,ename,salary*12 'annualSalary',hiredate
 FROM employee
