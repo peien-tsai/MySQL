fbc2272a540f463b8c4743b5053af22501afd0da
peien-tsai <59564956+peien-tsai@users.noreply.github.com>
2020-07-29 15:54:58 +0800

    加註解

--- workspace/m8_constrain.sql
+++ workspace/m8_constrain.sql
@@ -1,5 +1,5 @@
 CREATE TABLE emp1(
--- column level constraint
+-- column level constraint(限制寫在欄位的後面)
 empno	DECIMAL(4)	PRIMARY KEY,
 ename	VARCHAR(30)	NOT NULL,
 hiredate	DATE	NOT NULL,
@@ -7,7 +7,7 @@ email	VARCHAR(30)	UNIQUE,
 deptno	DECIMAL(3)	NOT NULL,
 salary	INT,
 title	VARCHAR(20)	NOT NULL DEFAULT 'engineer',
--- table level constraint
+-- table level constraint(限制寫在表格的最後面(所有欄位的後面))
 CONSTRAINT emp_deptno_fk  FOREIGN KEY(deptno)
 	REFERENCES department (deptno));
 
@@ -22,7 +22,7 @@ WHERE deptno = 500;
 INSERT INTO emp1
 VALUE (1001,'李大文', CURDATE(),'lee@gmail.com',200,89000, DEFAULT );
 
--- Error Code: 1062. Duplicate entry 'lee@gmail.com' for key 'email'
+-- Error Code: 1062. Duplicate entry 'lee@gmail.com' for key 'email' (重複輸入Unique欄位的email)
 INSERT INTO emp1
 VALUE (1002,'李大文', CURDATE(),'lee@gmail.com',200,89000, DEFAULT );
 
@@ -34,7 +34,7 @@ VALUE (1002,'李大三', CURDATE(),'lee@gmail.com',100,89000, DEFAULT );
 delete from department where deptno = 100 ;
 delete from department where deptno = 300 ;
 
--- altertablen→點FK→改成casecard
+-- altertable→點FK→改成casecard(一併跟著刪除或更新)
 ALTER TABLE `db01`.`emp1` 
 DROP FOREIGN KEY `emp_deptno_fk`;
 ALTER TABLE `db01`.`emp1` 
@@ -56,10 +56,10 @@ ADD CONSTRAINT `emp_deptno_fk`
 
 Delete FROM department where deptno = 201;
 
-
+-- AUTO_INCREMENT欄位自動編序號
 CREATE TABLE mem(
 	memno	int	PRIMARY KEY AUTO_INCREMENT,
-    mname	varchar(30)	NOT NULL);  -- 沒有預設初始值
+	mname	varchar(30)	NOT NULL);  -- 沒有預設初始值
 
 INSERT INTO mem(mname)
 VALUES('David Ho'),('Maria Eang'),('Pam Pan'),('Tina Lee'),('Jean Tsao');
@@ -68,7 +68,7 @@ Drop  TABLE mem;
 
 CREATE TABLE mem(
 	memno	int	PRIMARY KEY AUTO_INCREMENT,
-    mname	varchar(30)	NOT NULL)
+	mname	varchar(30)	NOT NULL)
 AUTO_INCREMENT = 100;  -- 預設初始值為100
 
 INSERT INTO mem(mname)
