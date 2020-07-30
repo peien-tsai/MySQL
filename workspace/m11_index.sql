ecbcb0897731c884eb271d84dbca29b7eb750896
peien-tsai <59564956+peien-tsai@users.noreply.github.com>
2020-07-30 14:49:45 +0800

    加註解

--- workspace/m11_index.sql
+++ workspace/m11_index.sql
@@ -1,5 +1,10 @@
+-- 顯示employee的index狀況
 SHOW INDEX FROM employee;
 SHOW INDEX FROM emp1;
 
+
+-- 建立索引 (為employee的ename欄位建立一個非唯一的索引)
 CREATE INDEX ename_idx ON employee (ename);
+-- 刪除index
 DROP INDEX ename_idx ON employee;
+ALTER TABLE employee DROP INDEX ename_idx;
